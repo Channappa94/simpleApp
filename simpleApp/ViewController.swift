f4554430b11c67b92ffca04e08a916e4ce13da3a
Phani_Kiran <pret321@gmail.com>
2019-07-17 17:42:02 -0500

    made simple app

--- simpleApp/ViewController.swift
+++ simpleApp/ViewController.swift
@@ -8,25 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var enterNumber: UITextField!
     @IBOutlet weak var myLabel: UILabel!
     
     @IBAction func myButton(_ sender: Any) {
-        let number = Int.random(in: 0...10)
+        let number = Int.random(in: 0...10)//Getting a random number.
         let text: String = enterNumber.text!
-        let numberFromString = Int(text)
-        if numberFromString == number {
+        let numberFromString = Int(text)!
+        if ((numberFromString > 10) || (numberFromString < 0)){//Checking if the number is greater than 10 and less that 0.
+            myLabel.text = "Number is invalid"
+        }else if numberFromString == number {//checking if the random number matches to the number from the string
             myLabel.text = "number matches"
         }
-        else{
+        else{//printing the number if the number does not match.
             myLabel.text = "number does not match"
         }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.enterNumber.delegate = self//Add this part
+        
         // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {//Function will help to toggle
+        self.view.endEditing(true)
     }
 
 
